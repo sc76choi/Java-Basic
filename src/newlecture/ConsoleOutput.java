2723c034504075da8918261504d5125f82dbaa0a
dasom037 <dasom037@gmail.com>
2022-05-02 23:12:23 +0900

    console output

---
+++ src/newlecture/ConsoleOutput.java
@@ -0,0 +1,10 @@
+package newlecture;
+
+public class ConsoleOutput {
+    public static void main(String[] args) {
+        System.out.write('3');
+        System.out.println();
+        System.out.write(51);
+        System.out.flush();
+    }
+}
---
+++ src/newlecture/ex1/io/Program.java
@@ -0,0 +1,22 @@
+package newlecture.ex1.io;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.write('3');
+        System.out.println();
+        System.out.write(51);
+        System.out.flush();
+        System.out.println();
+        System.out.write('3'+ 1);
+        System.out.write('3'+ 2);
+        System.out.write('3'+ 3);
+        System.out.write('3'+ 4);
+        System.out.flush();
+        System.out.println();
+        System.out.write('A'+ 1);
+        System.out.write('A'+ 2);
+        System.out.write('A'+ 3);
+        System.out.write('A'+ 4);
+        System.out.flush();
+    }
+}
