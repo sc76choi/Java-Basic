6cb1a94ac81f61e7ac378243ebf662e7107dbd09
dasom037 <dasom037@gmail.com>
2022-05-08 20:18:16 +0900

    add Variables

---
+++ src/javajungsuk3/ch06/Variables.java
@@ -0,0 +1,10 @@
+package javajungsuk3.ch06;
+
+public class Variables {
+    int iv; // 인스턴스 변수
+    static int cv; // 클래스 변수(static 변수, 공유변수)
+    
+    void method() {
+        int lv = 0; // 지역변수
+    }
+}
